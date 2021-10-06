951135a14317bde9d2755e58c1d9dc36db17b7b2
johnnywwy <johnnywwy@163.com>
2021-10-06 18:10:53 +0800

    完成了登录界面的功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
+// 注册全局可用组件
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+
+// 挂载Vue.prototype
+Vue.prototype.$message = Message
