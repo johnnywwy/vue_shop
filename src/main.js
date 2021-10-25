84c59ebf10f43902e2de6a3781126573157785dc
johnnywwy <johnnywwy@163.com>
2021-10-25 17:35:58 +0800

    完成商品功能的开发

--- src/main.js
+++ src/main.js
@@ -9,13 +9,21 @@ import './assets/fonts/iconfont.css'
 import './assets/css/global.css'
 // 安装 tree-grid 插件
 import TreeTable from 'vue-table-with-tree-grid'
+
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// 导入富文本编辑器样式
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
 // 导入axios
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 // 配置axios请求拦截器
 axios.interceptors.request.use(config => {
-  console.log(config)
+  // console.log(config)
   config.headers.Authorization = window.sessionStorage.getItem('token')
   // 最后必须 return config
   return config
@@ -25,7 +33,24 @@ Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
+Vue.filter('dataFormat', function(originVal) {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + 1 + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + 1 + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + 1 + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
 Vue.component('tree-table', TreeTable)
+
+// 将富文本编辑器，注册为全局可用的组件
+Vue.use(VueQuillEditor)
+
 new Vue({
   router,
   render: h => h(App)
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -28,7 +28,12 @@ import {
   Cascader,
   Alert,
   Tabs,
-  TabPane
+  TabPane,
+  Steps,
+  Step,
+  CheckboxGroup,
+  Checkbox,
+  Upload
 } from 'element-ui'
 
 // 注册全局可用组件
@@ -59,6 +64,11 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Steps)
+Vue.use(Step)
+Vue.use(CheckboxGroup)
+Vue.use(Checkbox)
+Vue.use(Upload)
 
 // 挂载Vue.prototype
 Vue.prototype.$message = Message
